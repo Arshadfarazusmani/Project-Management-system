cfe56082964750b0e1c7259badb84c78651af355
Arshad Faraz <arshadfarazusmani@gmail.com>
2025-08-13 20:37:40 +0530

    Add cookie-parser and cors middleware to enhance request handling

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,35 @@
 import express from "express";
+import cookieParser from "cookieparser";
+import cors from "cors"
 
 const app = express() // creating express application -> object which have multiple functionality
+
+//  configuring app to use cors middleware.
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+    
+}));
+// configuring app to use json middleware.
+app.use(express.json({
+    limit: '16kb'
+}));
+// configuring app to use urlencoded middleware.
+app.use(express.urlencoded({
+    extended: true,
+    limit: '16kb'
+}));
+// configuring app to use static middleware.
+// serve static files from the 'public' directory.
+
+app.use(express.static('public'));
+
+
+// configuring app to use cookie-parser middleware.
+// cookie-parser is used to parse cookies attached to the client request object.
+
+app.use(cookieParser());
+
 
 export {app}
